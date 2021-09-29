2a89109c98986dbdd9ea6f81cae9aaaf4f0d450c
Michael Halbrook <halbrookmichael@gmail.com>
2021-09-29 07:06:53 -0700

    adding amplify for deployment

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
