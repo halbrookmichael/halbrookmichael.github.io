05dfc0edd86fabcb758bc31f0cd4776e3a09742a
Michael Halbrook <halbrookmichael@gmail.com>
2021-10-04 10:08:05 -0700

    update email in footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = (props) => {
       </section>
       <section class="footer">
         <div className="footer-content">
-          <p className="email">Email: <a href="mailto:info@altered-pixels.tech">info@altered-pixels.tech</a></p>
+          <p className="email">Email: <a href="mailto:info@altered-pixels.tech">halbrookmichael@gmail.com</a></p>
           <p className="copyright">&copy; Altered Pixels. All rights reserved.</p>
           <p>Phone: <a className="phone" href="tel:509-771-1384"><span>509.771.1384</span></a></p>
         </div>
